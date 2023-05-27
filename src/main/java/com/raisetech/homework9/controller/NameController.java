b00d60ef081f6bdf65272c29245cea9c539f60e7
chaki1003 <jihongchamu439@gmail.com>
2023-05-27 17:29:21 +0900

    第九回課題修正

--- src/main/java/com/raisetech/homework9/controller/NameController.java
+++ src/main/java/com/raisetech/homework9/controller/NameController.java
@@ -12,13 +12,13 @@ import java.util.List;
 public class NameController {
 
     private final NameService nameService;
+
     public NameController(NameService nameService){
         this.nameService = nameService;
     }
 
     @GetMapping("/names")
-    public List<NameResponse>getNames(){
-        return nameService.findAll().stream().map(NameResponse::new).toList();
+    public List<NameResponse> getNames() {return nameService.findAll().stream().map(NameResponse::new).toList();
     }
 
 }
